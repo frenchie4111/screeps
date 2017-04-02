9fa472563abbb001ab23752511bb9fb4028384e0
Michael Lyons <mdl0394@gmail.com>
2017-04-02 11:15:46 -0700

    Made it find the closest source

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ module.exports.loop = function() {
 
         if( creep.carry.energy < creep.carryCapacity ) {
             let sources = creep.room.find( FIND_SOURCES );
-            if( creep.harvest( sources[ 0 ] ) === ERR_NOT_IN_RANGE ) {
-                creep.moveTo( sources[ 0 ] );
+            source = creep.pos.findClosestByPath( sources );
+
+            if( creep.harvest( source ) === ERR_NOT_IN_RANGE ) {
+                creep.moveTo( source );
             }
         } else {
             if( creep.transfer( Game.spawns[ 'Spawn1' ], RESOURCE_ENERGY ) === ERR_NOT_IN_RANGE ) {
