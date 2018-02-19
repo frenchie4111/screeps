438b24be95129c980a4a16c1e1ef8eebb5926183
Michael Lyons <mdl0394@gmail.com>
2018-02-19 15:50:26 -0800

    Removed print

--- src/metrics/MetricCollector.js
+++ src/metrics/MetricCollector.js
@@ -8,7 +8,6 @@ class MetricCollector {
     }
 
     getMemory( room ) {
-        console.log( 'getMemory', room );
         if( !room.memory.hasOwnProperty( '_metrics' ) ) room.memory._metrics = {};
         if( !room.memory._metrics.hasOwnProperty( this.name ) ) room.memory._metrics[ this.name ] = {};
         return room.memory._metrics[ this.name ];
