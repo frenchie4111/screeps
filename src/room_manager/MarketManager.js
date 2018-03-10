19b79123e494295399028cdcebc64821d46e3e4e
Michael Lyons <mdl0394@gmail.com>
2018-03-10 09:24:27 -0800

    made market more generic

--- src/room_manager/MarketManager.js
+++ src/room_manager/MarketManager.js
@@ -6,48 +6,66 @@ const constants = require( '~/constants' ),
 
 const Assigner = require( './Assigner' );
 
+const REASONABLE_PRICES = {
+    [ RESOURCE_ENERGY ]: 0.029,
+    [ RESOURCE_LEMERGIUM ]: 0.110
+};
+
 class MarketManager {
     getMemory( room ) {
         let memory = room.memory.market = room.memory.market || {};
         return memory;
     }
 
-    doManage( room, spawn ) {
-        let memory = this.getMemory( room );
-        console.log( 'Market' );
-
-        let current_energy = room.terminal.store[ RESOURCE_ENERGY ];
-        if( current_energy < 100000 ) return;
-        if( room.terminal.cooldown > 0 ) return;
-
+    getBestBuyOrderFor( room, resource_type, quantity, current_energy ) {
         let orders = Game.market
             .getAllOrders( ( order ) => {
                 return (
-                    order.resourceType == RESOURCE_ENERGY &&
+                    order.resourceType == resource_type &&
+                    REASONABLE_PRICES.hasOwnProperty( resource_type ) &&
                     order.type == ORDER_BUY &&
-                    order.price > 0.029 &&
-                    order.amount >= 10000 &&
-                    Game.market.calcTransactionCost( 10000, room.name, order.roomName ) < 10000 &&
-                    ( Game.market.calcTransactionCost( 10000, room.name, order.roomName ) + order.amount ) < current_energy
+                    order.price > REASONABLE_PRICES[ resource_type ] &&
+                    order.amount >= quantity &&
+                    Game.market.calcTransactionCost( quantity, room.name, order.roomName ) < 10000 &&
+                    ( Game.market.calcTransactionCost( quantity, room.name, order.roomName ) + ( resource_type === RESOURCE_ENERGY ? order.amount : 0 ) ) < current_energy
                 )
             } );
 
         orders = _
             .forEach( orders, ( order ) => {
-                let order_total_eng_cost = ( 10000 + Game.market.calcTransactionCost( 10000, room.name, order.roomName ) );
-                let total_profit = order.price * 10000;
-                let per_eng_profit = total_profit / order_total_eng_cost;
-                order.per_eng_profit = per_eng_profit;
+                // Cost of transaction evaluated using reasonable price for energy
+                let transaction_cost_eng = Game.market.calcTransactionCost( quantity, room.name, order.roomName );
+                let transaction_cost_credits = REASONABLE_PRICES[ RESOURCE_ENERGY ] * transaction_cost_eng;
+
+                let profit = order.price * quantity;
+                let per_resource_profit = profit / ( profit + transaction_cost_credits );
+
+                order.transaction_cost_eng = transaction_cost_eng;
+                order.per_resource_profit = per_resource_profit;
             } );
 
-        orders = _.sortBy( orders, order => order.per_eng_profit );
+        orders = _.sortBy( orders, order => order.per_resource_profit );
         orders.reverse();
         orders = orders.slice( 0, 10 );
 
-        let order = orders[ 0 ];
+        return orders[ 0 ];
+    }
+
+    // handleRoomState : doManage-E44S3 : market : 
+    // false 
+    // {"created":4165479,"type":"buy","amount":10000,"remainingAmount":10000,"resourceType":"energy","price":0.031,"roomName":"W1S19","id":"5a226d6ef30e5c2ce1d2c582","transaction_cost_eng":7842,"per_eng_profit":0.017374733774240557,"per_resource_profit":0.5768321864917066} {"created":6528896,"type":"buy","amount":242550,"remainingAmount":8992810,"resourceType":"energy","price":0.03,"roomName":"E9N21","id":"5a9a249d621ca73755b491c0","transaction_cost_eng":6886,"per_resource_profit":0.6003674248640167}
+
+
+    doManage( room, spawn ) {
+        let memory = this.getMemory( room );
+
+        let current_energy = room.terminal.store[ RESOURCE_ENERGY ];
+        if( current_energy < 100000 ) return;
+        if( room.terminal.cooldown > 0 ) return;
+
+        let order = this.getBestBuyOrderFor( room, RESOURCE_ENERGY, 10000, current_energy );
 
         let response = Game.market.deal( order.id, 10000, room.name );
-
         console.log( 'Market response ', response, constants.lookup( response ) );
     }
 }
