f72cea34abb14b556e43212c9b8890b257c3f0e9
Michael Lyons <mdl0394@gmail.com>
2018-03-18 12:07:49 -0700

    Made stats write to segment

--- src/stats.js
+++ src/stats.js
@@ -3,11 +3,11 @@ const flat = require( 'flat' );
 let profiler = require( '~/profiler' );
 
 module.exports = () => {
-    // Put in your main loop
+    RawMemory.setActiveSegments( [ 99 ] );
 
     const stats_cpu_start = Game.cpu.getUsed();
 
-    Memory.stats = {}
+    stats = {}
 
     let rooms = Game.rooms;
     let spawns = Game.spawns;
@@ -22,29 +22,29 @@ module.exports = () => {
 
         let is_my_room = ( room.controller ? room.controller.my : 0 );
         if( room_type === 'standard' ) {
-            Memory.stats[ 'room.' + room.name + '.myRoom' ] = 1;
-            Memory.stats[ 'room.' + room.name + '.energyAvailable' ] = room.energyAvailable;
-            Memory.stats[ 'room.' + room.name + '.energyCapacityAvailable' ] = room.energyCapacityAvailable;
-            Memory.stats[ 'room.' + room.name + '.controllerProgress' ] = room.controller.progress;
-            Memory.stats[ 'room.' + room.name + '.controllerProgressTotal' ] = room.controller.progressTotal;
-            Memory.stats[ 'room.' + room.name + '.creep_count' ] = room.memory.creeps.length;
+            stats[ 'room.' + room.name + '.myRoom' ] = 1;
+            stats[ 'room.' + room.name + '.energyAvailable' ] = room.energyAvailable;
+            stats[ 'room.' + room.name + '.energyCapacityAvailable' ] = room.energyCapacityAvailable;
+            stats[ 'room.' + room.name + '.controllerProgress' ] = room.controller.progress;
+            stats[ 'room.' + room.name + '.controllerProgressTotal' ] = room.controller.progressTotal;
+            stats[ 'room.' + room.name + '.creep_count' ] = room.memory.creeps.length;
             let stored = 0;
             let storedTotal = 0;
 
             if( room.storage ) {
                 stored = room.storage.store[ RESOURCE_ENERGY ]
                 storedTotal = room.storage.storeCapacity[ RESOURCE_ENERGY ]
-                Memory.stats[ 'room.' + room.name + '.storage.store' ]  = room.storage.store;
+                stats[ 'room.' + room.name + '.storage.store' ]  = room.storage.store;
             } else {
                 stored = 0
                 storedTotal = 0
             }
 
             if( room.terminal ) {
-                Memory.stats[ 'room.' + room.name + '.terminal.store' ]  = room.terminal.store;
+                stats[ 'room.' + room.name + '.terminal.store' ]  = room.terminal.store;
             }
 
-            Memory.stats[ 'room.' + room.name + '.storedEnergy' ] = stored
+            stats[ 'room.' + room.name + '.storedEnergy' ] = stored
 
             let creeps = _
                 .reduce( room.memory.creeps, ( full, creep ) => {
@@ -58,11 +58,11 @@ module.exports = () => {
                     return full;
                 }, {} );
 
-            Memory.stats[ 'room.' + room.name + '.creeps' ] = creeps;
+            stats[ 'room.' + room.name + '.creeps' ] = creeps;
         }
         if( room_type === 'long_distance' ) {
-            let ldm_rooms = Memory.stats.ldm_room = Memory.stats.ldm_room || {};
-            let ldm_room = Memory.stats.ldm_room[ room.name ] = {};
+            let ldm_rooms = stats.ldm_room = stats.ldm_room || {};
+            let ldm_room = stats.ldm_room[ room.name ] = {};
 
             ldm_room.source_containers = _
                 .reduce( room.memory.source_containers, ( full, source_container, source_id ) => {
@@ -76,15 +76,15 @@ module.exports = () => {
         }
     }
 
-    Memory.stats[ 'gcl.progress' ] = Game.gcl.progress
-    Memory.stats[ 'gcl.progressTotal' ] = Game.gcl.progressTotal
-    Memory.stats[ 'gcl.level' ] = Game.gcl.level
+    stats[ 'gcl.progress' ] = Game.gcl.progress
+    stats[ 'gcl.progressTotal' ] = Game.gcl.progressTotal
+    stats[ 'gcl.level' ] = Game.gcl.level
 
-    Memory.stats[ 'profiler' ] = profiler.getProfile();    
+    stats[ 'profiler' ] = profiler.getProfile();    
 
     for ( let spawnKey in spawns ) {
         let spawn = Game.spawns[ spawnKey ]
-        Memory.stats[ 'spawn.' + spawn.name + '.defenderIndex' ] = spawn.memory[ 'defenderIndex' ]
+        stats[ 'spawn.' + spawn.name + '.defenderIndex' ] = spawn.memory[ 'defenderIndex' ]
     }
 
     let creeps = _
@@ -96,13 +96,15 @@ module.exports = () => {
             }
             return full;
         }, {} );
-    Memory.stats.creeps = creeps;
+    stats.creeps = creeps;
 
-    Memory.stats.credits = Game.market.credits;
+    stats.credits = Game.market.credits;
 
-    Memory.stats = flat( Memory.stats );
-    
-    Memory.stats.stats_cpu = Game.cpu.getUsed() - stats_cpu_start;
-    Memory.stats.cpu_used = Game.cpu.getUsed();
-    Memory.stats.cpu_bucket = Game.cpu.bucket;
+    stats = flat( stats );
+
+    stats.stats_cpu = Game.cpu.getUsed() - stats_cpu_start;
+    stats.cpu_used = Game.cpu.getUsed();
+    stats.cpu_bucket = Game.cpu.bucket;
+
+    RawMemory.segments[ 99 ] = JSON.stringify( stats );
 }
